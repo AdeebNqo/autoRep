4fb00ccff0e9ac4d1fad6d60cea76ede6b420bd3
AdeebNqo <adeebnqo@gmail.com>
2013-05-13 05:26:47 +0200

    tired, buggy

--- gen/com/adeeb/autorep/R.java
+++ gen/com/adeeb/autorep/R.java
@@ -14,11 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f070002;
-        public static final int editText1=0x7f070001;
-        public static final int menu_settings=0x7f070004;
-        public static final int textView1=0x7f070000;
-        public static final int toggleButton1=0x7f070003;
+        public static final int alt_num=0x7f070004;
+        public static final int button1=0x7f070005;
+        public static final int editText1=0x7f070002;
+        public static final int menu_settings=0x7f070006;
+        public static final int textView1=0x7f070001;
+        public static final int textView2=0x7f070003;
+        public static final int toggleButton1=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main_screen=0x7f030000;
@@ -27,10 +29,13 @@ public final class R {
         public static final int activity_main_screen=0x7f060000;
     }
     public static final class string {
+        public static final int alternative=0x7f040005;
         public static final int app_name=0x7f040000;
         public static final int done=0x7f040004;
         public static final int hello_world=0x7f040002;
         public static final int menu_settings=0x7f040001;
+        public static final int name=0x7f040006;
+        public static final int number=0x7f040007;
         public static final int username=0x7f040003;
     }
     public static final class style {
--- src/com/adeeb/autorep/Main_screen.java
+++ src/com/adeeb/autorep/Main_screen.java
@@ -11,14 +11,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Main_screen extends Activity {
+public class Main_screen extends Activity implements OnClickListener {
 
 	static int missed_call = 0;
 	static int num_missed_calls = 0;
 	static TelephonyManager tm;
 	static String username;
+	static String other_num;
 	
 	EditText username_text;
+	EditText number_text;
 	Button save_button;
 	
 	@Override
@@ -29,13 +31,9 @@ public class Main_screen extends Activity {
 		//Toast.makeText(getBaseContext(), "before tm", Toast.LENGTH_LONG).show();
 		tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
 		username_text = (EditText)findViewById(R.id.editText1);
+		number_text = (EditText)findViewById(R.id.alt_num);
 		save_button = (Button)findViewById(R.id.button1);
-		save_button.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(getBaseContext(), "Done", Toast.LENGTH_LONG).show();
-				username = username_text.getText().toString();
-			}});
+		save_button.setOnClickListener(this);
 	}
 
 	@Override
@@ -43,6 +41,13 @@ public class Main_screen extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main_screen, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		Toast.makeText(getBaseContext(), "Done", Toast.LENGTH_LONG).show();
+		username = username_text.getText().toString();
+		other_num = number_text.getText().toString();
 	}
 
 }
